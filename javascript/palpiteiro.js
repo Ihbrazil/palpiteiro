9a15461d7c9ae2c80a2ff9283b5562f3c355adfb
ihbrazil <ihbrazil@users.noreply.github.com>
2023-10-27 21:06:39 -0300

    atualizacao-flex

--- javascript/palpiteiro.js
+++ javascript/palpiteiro.js
@@ -1,12 +1,221 @@
-function palpite ()
+// Mega-sena
+function palpiteMega ()
         {
             do {
                 var numero_sorteado = Math.floor(Math.random() * 61);
-            } while ((numero_sorteado === 0) || (numero_sorteado > 60)); 
-            return (numero_sorteado);
+            } while ((numero_sorteado === 0) || (numero_sorteado > 60));
+           /* document.getElementById("numeroUm").innerHTML = numero_sorteado;*/
+           return(numero_sorteado); 
+        }
+        
+        function palpiteInicialMega ()
+        {
+            document.getElementById("numeroUm").innerHTML = palpiteMega();
+            
+            document.getElementById("numeroDois").innerHTML = palpiteMega();
+            
+            document.getElementById("numeroTres").innerHTML = palpiteMega();
+            
+            document.getElementById("numeroQuatro").innerHTML = palpiteMega(); 
+            
+            document.getElementById("numeroCinco").innerHTML = palpiteMega();
+            
+            document.getElementById("numeroSeis").innerHTML = palpiteMega();
         }
 
-function atualizarPagina()
+        function palpiteUmMega ()
         {
-            window.location.reload(); 
+            document.getElementById("numeroUm").innerHTML = palpiteMega(); 
+        }
+
+        function palpiteDoisMega ()
+        {
+            document.getElementById("numeroDois").innerHTML = palpiteMega(); 
+        }
+
+        function palpiteTresMega ()
+        {
+            document.getElementById("numeroTres").innerHTML = palpiteMega(); 
+        }
+
+        function palpiteQuatroMega ()
+        {
+            document.getElementById("numeroQuatro").innerHTML = palpiteMega(); 
+        }
+
+        function palpiteCincoMega ()
+        {
+            document.getElementById("numeroCinco").innerHTML = palpiteMega(); 
+        }
+
+        function palpiteSeisMega ()
+        {
+            document.getElementById("numeroSeis").innerHTML = palpiteMega(); 
+        }
+
+
+// Quina
+
+function palpiteQuina ()
+        {
+            do {
+                var numero_sorteado = Math.floor(Math.random() * 81);
+            } while ((numero_sorteado === 0) || (numero_sorteado > 80));
+           /* document.getElementById("numeroUm").innerHTML = numero_sorteado;*/
+           return(numero_sorteado); 
+        }
+        
+        function palpiteInicialQuina ()
+        {
+            document.getElementById("numeroUm").innerHTML = palpiteQuina();
+            
+            document.getElementById("numeroDois").innerHTML = palpiteQuina();
+            
+            document.getElementById("numeroTres").innerHTML = palpiteQuina();
+            
+            document.getElementById("numeroQuatro").innerHTML = palpiteQuina(); 
+            
+            document.getElementById("numeroCinco").innerHTML = palpiteQuina();
+        }
+
+        function palpiteUmQuina ()
+        {
+            document.getElementById("numeroUm").innerHTML = palpiteQuina(); 
+        }
+
+        function palpiteDoisQuina ()
+        {
+            document.getElementById("numeroDois").innerHTML = palpiteQuina(); 
+        }
+
+        function palpiteTresQuina ()
+        {
+            document.getElementById("numeroTres").innerHTML = palpiteQuina(); 
+        }
+
+        function palpiteQuatroQuina ()
+        {
+            document.getElementById("numeroQuatro").innerHTML = palpiteQuina(); 
+        }
+
+        function palpiteCincoQuina ()
+        {
+            document.getElementById("numeroCinco").innerHTML = palpiteQuina(); 
+        }
+
+// LotoFácil
+
+function palpiteLotoFacil ()
+        {
+            do {
+                var numero_sorteado = Math.floor(Math.random() * 26);
+            } while ((numero_sorteado === 0) || (numero_sorteado > 25));
+           /* document.getElementById("numeroUm").innerHTML = numero_sorteado;*/
+           return(numero_sorteado); 
+        }
+        
+        function palpiteInicialLotoFacil ()
+        {
+            document.getElementById("numeroUm").innerHTML = palpiteLotoFacil();
+            
+            document.getElementById("numeroDois").innerHTML = palpiteLotoFacil();
+            
+            document.getElementById("numeroTres").innerHTML = palpiteLotoFacil();
+            
+            document.getElementById("numeroQuatro").innerHTML = palpiteLotoFacil(); 
+            
+            document.getElementById("numeroCinco").innerHTML = palpiteLotoFacil();
+            
+            document.getElementById("numeroSeis").innerHTML = palpiteLotoFacil();
+
+            document.getElementById("numeroSete").innerHTML = palpiteLotoFacil();
+            
+            document.getElementById("numeroOito").innerHTML = palpiteLotoFacil();
+            
+            document.getElementById("numeroNove").innerHTML = palpiteLotoFacil();
+            
+            document.getElementById("numeroDez").innerHTML = palpiteLotoFacil(); 
+            
+            document.getElementById("numeroOnze").innerHTML = palpiteLotoFacil();
+            
+            document.getElementById("numeroDoze").innerHTML = palpiteLotoFacil();
+
+            document.getElementById("numeroTreze").innerHTML = palpiteLotoFacil();
+            
+            document.getElementById("numeroQuatorze").innerHTML = palpiteLotoFacil();
+            
+            document.getElementById("numeroQuinze").innerHTML = palpiteLotoFacil();
+        }
+
+        function palpiteUmLotoFacil ()
+        {
+            document.getElementById("numeroUm").innerHTML = palpiteLotoFacil(); 
+        }
+
+        function palpiteDoisLotoFacil ()
+        {
+            document.getElementById("numeroDois").innerHTML = palpiteLotoFacil(); 
+        }
+
+        function palpiteTresLotoFacil ()
+        {
+            document.getElementById("numeroTres").innerHTML = palpiteLotoFacil(); 
+        }
+
+        function palpiteQuatroLotoFacil ()
+        {
+            document.getElementById("numeroQuatro").innerHTML = palpiteLotoFacil(); 
+        }
+
+        function palpiteCincoLotoFacil ()
+        {
+            document.getElementById("numeroCinco").innerHTML = palpiteLotoFacil(); 
+        }
+        function palpiteSeisLotoFacil ()
+        {
+            document.getElementById("numeroSeis").innerHTML = palpiteLotoFacil(); 
+        }
+
+        function palpiteSeteLotoFacil ()
+        {
+            document.getElementById("numeroSete").innerHTML = palpiteLotoFacil(); 
+        }
+
+        function palpiteOitoLotoFacil ()
+        {
+            document.getElementById("numeroOito").innerHTML = palpiteLotoFacil(); 
+        }
+
+        function palpiteNoveLotoFacil ()
+        {
+            document.getElementById("numeroNove").innerHTML = palpiteLotoFacil(); 
+        }
+
+        function palpiteDezLotoFacil ()
+        {
+            document.getElementById("numeroDez").innerHTML = palpiteLotoFacil(); 
+        }
+        function palpiteOnzeLotoFacil ()
+        {
+            document.getElementById("numeroOnze").innerHTML = palpiteLotoFacil(); 
+        }
+
+        function palpiteDozeLotoFacil ()
+        {
+            document.getElementById("numeroDoze").innerHTML = palpiteLotoFacil(); 
+        }
+
+        function palpiteTrezeLotoFacil ()
+        {
+            document.getElementById("numeroTreze").innerHTML = palpiteLotoFacil(); 
+        }
+
+        function palpiteQuatorzeLotoFacil ()
+        {
+            document.getElementById("numeroQuatorze").innerHTML = palpiteLotoFacil(); 
+        }
+
+        function palpiteQuinzeLotoFacil ()
+        {
+            document.getElementById("numeroQuinze").innerHTML = palpiteLotoFacil(); 
         }
